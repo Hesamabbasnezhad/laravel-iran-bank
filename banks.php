91c4c47bb3b3219b7a7251c068092a521b20be17
Hesam Abbasnezhad <hessam.abbasnezhad@gmail.com>
2025-11-15 00:39:35 +0330

    Add Banks.php

---
+++ banks.php
@@ -0,0 +1,132 @@
+<?php
+
+return [
+
+    'bank_cards' => [
+        [
+            'card_no' => 627381,
+            'bank_name' => 'ansar',
+            'bank_title' => 'بانک انصار',
+            'color' => '#26eeb900',
+        ],
+        [
+            'card_no' => 636214,
+            'bank_name' => 'ayandeh',
+            'bank_title' => 'بانک آینده',
+            'color' => '#3388391d',
+        ],
+        [
+            'card_no' => 502938,
+            'bank_name' => 'dey',
+            'bank_title' => 'بانک دی',
+            'color' => '#0dffffff',
+        ],
+        [
+            'card_no' => 627412,
+            'bank_name' => 'eghtesad_novin',
+            'bank_title' => 'بانک اقتصادنوین',
+            'color' => '#1a662891',
+        ],
+        [
+            'card_no' => 628157,
+            'bank_name' => 'etebari_tosee',
+            'bank_title' => 'موسسه اعتباری توسعه',
+            'color' => '#33000000',
+        ],
+        [
+            'card_no' => 603769,
+            'bank_name' => 'saderat',
+            'bank_title' => 'بانک صادرات',
+            'color' => '#004ec2',
+        ],
+        [
+            'card_no' => 621986,
+            'bank_name' => 'saman',
+            'bank_title' => 'بانک سامان',
+            'color' => '#1a7dcdf1',
+        ],
+    ],
+
+    'iban_banks' => [
+        [
+            'nickname' => 'central-bank',
+            'name' => 'Central Bank of Iran',
+            'persian_name' => 'بانک مرکزی جمهوری اسلامی ایران',
+            'code' => '010',
+            'account_number_available' => false,
+        ],
+        [
+            'nickname'=> 'sanat-o-madan',
+            'name'=> 'Sanat O Madan Bank',
+            'persianName'=> 'بانک صنعت و معدن',
+            'code'=> '011',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'mellat',
+            'name'=> 'Mellat Bank',
+            'persianName'=> 'بانک ملت',
+            'code'=> '012',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'refah',
+            'name'=> 'Refah Bank',
+            'persianName'=> 'بانک رفاه کارگران',
+            'code'=> '013',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'maskan',
+            'name'=> 'Maskan Bank',
+            'persianName'=> 'بانک مسکن',
+            'code'=> '014',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'sepah',
+            'name'=> 'Sepah Bank',
+            'persianName'=> 'بانک سپه',
+            'code'=> '015',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'keshavarzi',
+            'name'=> 'Keshavarzi',
+            'persianName'=> 'بانک کشاورزی',
+            'code'=> '016',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'melli',
+            'name'=> 'Melli',
+            'persianName'=> 'بانک ملی ایران',
+            'code'=> '017',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'tejarat',
+            'name'=> 'Tejarat Bank',
+            'persianName'=> 'بانک تجارت',
+            'code'=> '018',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'saderat',
+            'name'=> 'Saderat Bank',
+            'persianName'=> 'بانک صادرات ایران',
+            'code'=> '019',
+            'accountNumberAvailable'=> false,
+        ],
+        [
+            'nickname'=> 'tosee-saderat',
+            'name'=> 'Tose Saderat Bank',
+            'persianName'=> 'بانک توسعه صادرات',
+            'code'=> '020',
+            'accountNumberAvailable'=> false,
+        ],
+
+
+    ],
+
+];
